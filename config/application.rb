6a3b14ce8e7c52bb16f39aed4a617696e8250a8c
jouhri <jouhri.oualid@gmail.com>
2015-03-06 17:01:28 +0100

    add spec for model user

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,12 @@ Bundler.require(*Rails.groups)
 
 module Booostme
   class Application < Rails::Application
+
+    config.generators do |g|
+        g.test_framework :rspec, fixture: true
+        g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :user do
+    first_name { Faker::Name.first_name }
+    last_name  { Faker::Name.last_name }
+    password  { Faker::Internet.password }
+    email { Faker::Internet.email}
+    description {Faker::Hacker.say_something_smart}
+    avatar ""
+  end
+
+end
