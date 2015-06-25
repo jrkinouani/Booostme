d1cb76ecf7786f24ae0a0c8dcf2090427e395b22
jrkinouani <kinouani@gmail.com>
2015-06-25 12:28:04 +0400

    create btn learn more

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150509142529) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "img"
     t.string   "image"
   end
 
@@ -35,10 +36,10 @@ ActiveRecord::Schema.define(version: 20150509142529) do
     t.integer  "hour"
     t.string   "validation_image"
     t.string   "cover_image"
-    t.string   "state",            default: "ongoing"
+    t.string   "state",            default: "to_do"
     t.integer  "user_id"
-    t.datetime "created_at",                           null: false
-    t.datetime "updated_at",                           null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
   end
 
   add_index "tasks", ["user_id"], name: "index_tasks_on_user_id"
